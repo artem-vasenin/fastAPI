8c43d1aa48bd6f5dd4d33ab8ba8f2055f075468c
Rusich <vasenin@verqor.com>
2025-04-01 16:21:15 +0300

    added py validation

--- src/mocks.py
+++ src/mocks.py
@@ -1,10 +1,12 @@
+import datetime
+
 tasks = [
-    {'id': 1, 'cid': 1, 'name': 'Task 1', 'cnt': 3},
-    {'id': 2, 'cid': 1, 'name': 'Task 2', 'cnt': 3},
-    {'id': 3, 'cid': 2, 'name': 'Task 3', 'cnt': 6},
-    {'id': 4, 'cid': 2, 'name': 'Task 4', 'cnt': 3},
-    {'id': 5, 'cid': 3, 'name': 'Task 5', 'cnt': 1},
-    {'id': 6, 'cid': 3, 'name': 'Task 6', 'cnt': 3},
+    {'id': 1, 'cid': 1, 'name': 'Task 1', 'cnt': 3, 'created_at': datetime.datetime(2023, 10, 12)},
+    {'id': 2, 'cid': 1, 'name': 'Task 2', 'cnt': 3, 'created_at': datetime.datetime(2023, 10, 12)},
+    {'id': 3, 'cid': 2, 'name': 'Task 3', 'cnt': 6, 'created_at': datetime.datetime(2023, 10, 12)},
+    {'id': 4, 'cid': 2, 'name': 'Task 4', 'cnt': 3, 'created_at': datetime.datetime(2023, 10, 12)},
+    {'id': 5, 'cid': 3, 'name': 'Task 5', 'cnt': 1, 'created_at': datetime.datetime(2023, 10, 12)},
+    {'id': 6, 'cid': 3, 'name': 'Task 6', 'cnt': 3, 'created_at': datetime.datetime(2023, 10, 12)},
 ]
 
 cats = [
--- src/schemas/task.py
+++ src/schemas/task.py
@@ -1,8 +1,11 @@
-from pydantic import BaseModel
+from datetime import datetime
+
+from pydantic import BaseModel, Field
 
 
 class Task(BaseModel):
     id: int
-    cid: int
+    cid: int | None = None
     name: str
     cnt: int
+    created_at: datetime = Field(exclude=True)
