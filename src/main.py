b0cebc47d27c3b55df11ed1e9b67043952cad7d3
Rusich <vasenin@verqor.com>
2026-01-18 21:04:43 +0300

    added first test api

--- src/main.py
+++ src/main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+from fastapi import FastAPI, status, HTTPException
+from pydantic import BaseModel
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
+class Msg(BaseModel):
+    id: int
+    content: str
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+msg_db: list[Msg] = [Msg(id=0, content='Hello Rusich')]
 
+@app.get('/msg', response_model=list[Msg])
+async def get_list()->list[Msg]:
+    return msg_db
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get('/msg/{idx}', response_model=Msg)
+async def get_item(idx: int)->Msg:
+    for m in msg_db:
+        if m.id == idx:
+            return m
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Item is not found')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.post('/msg', response_model=list[Msg], status_code=status.HTTP_201_CREATED)
+async def add_msg(msg: Msg)->list[Msg]:
+    if any(m.id == msg.id for m in msg_db):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Msg already exist')
+    msg_db.append(msg)
+    return msg_db
+
+@app.put('/msg/{idx}', response_model=Msg)
+async def update_msg(msg: Msg, idx: int)->Msg:
+    if msg.id != idx:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="id is not eq")
+    for i, m in enumerate(msg_db):
+        if m.id == idx:
+            msg_db[i] = msg
+            return msg
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Msg not found")
+
+@app.delete('/msg/{idx}')
+async def del_msg(idx: int)->list[Msg]:
+    for i, m in enumerate(msg_db):
+        if m.id == idx:
+            del msg_db[i]
+            return msg_db
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Msg not found")
