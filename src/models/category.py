52892586f79ea2a6c3539f6d0c36e289c20744f1
Rusich <vasenin@verqor.com>
2026-01-25 17:34:01 +0300

    added relations

---
+++ src/models/category.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Boolean, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.database import Base
+
+from .product import Product
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    parent_id: Mapped[int | None] = mapped_column(
+        ForeignKey("categories.id"), nullable=True
+    )
+
+    products: Mapped[list["Product"]] = relationship(
+        "Product", back_populates="category"
+    )
+    parent: Mapped["Category | None"] = relationship(
+        "Category", back_populates="children", remote_side="Category.id"
+    )
+    children: Mapped[list["Category"]] = relationship(
+        "Category", back_populates="parent"
+    )
